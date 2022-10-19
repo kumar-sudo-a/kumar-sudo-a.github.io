00083596902e057d18802df0a9b3a69982263b1e
Sharath Kumar <kumaras.java@gmail.com>
2022-10-18 22:06:54 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+console.log("Namasthe", "🙏🏻");
+console.log('Thank you for stalking my code.', "💪🏻");
+console.log('Adios! Amigos', "👋🏻");
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+    setTheme('light')
+}else{
+    setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for (var i=0; themeDots.length > 1; i++){
+    themeDots[i].addEventListener('click', function(){
+        let mode = this.dataset.mode
+        console.log('Option Clicked', mode)
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'css/default.css'
+	}
+
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href = 'css/blue.css'
+	}
+
+	if(mode == 'green'){
+		document.getElementById('theme-style').href = 'css/green.css'
+	}
+
+	if(mode == 'purple'){
+		document.getElementById('theme-style').href = 'css/purple.css'
+	}
+
+	localStorage.setItem('theme', mode)
+}
